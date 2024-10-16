3d29551ad29cc6b234192d5ee448ab5566a3055a
codebluenick <codebluenick@gmail.com>
2024-10-16 01:29:11 -0700

    Remove unnecessary codes

--- docs/tictactoe.js
+++ docs/tictactoe.js
@@ -12,27 +12,14 @@ const ctx = canvas.getContext("2d");
 // UI references
 const restartButton = document.querySelector("#restart");
 const undoButton = document.querySelector('#undo');
-const rotateButton = document.querySelector('#rotate');
-const colorSelectButtons = document.querySelectorAll(".color-select");
-const playerScoreText = document.querySelector('#score-text'); 
 
 // Constants
-const CELL_COLORS = {
-    white: [255, 255, 255],
-    black: [0, 0, 0],
-    red: [255, 0, 0],
-    green: [0, 255, 0], 
-    blue: [0, 0, 255]
-}
-const CELLS_PER_AXIS = 9;
+const CELLS_PER_AXIS = 3;
 const CELL_WIDTH = canvas.width/CELLS_PER_AXIS;
 const CELL_HEIGHT = canvas.height/CELLS_PER_AXIS;
-const MAXIMUM_SCORE = CELLS_PER_AXIS * CELLS_PER_AXIS;;
 
 // Game objects
-let replacementColor = CELL_COLORS.white;
-let grids;
-let playerScore = MAXIMUM_SCORE;
+
 
 // #endregion
 
@@ -68,23 +55,6 @@ function rollBackHistory() {
     }
 }
 
-function transposeGrid() {
-    for (let i = 0; i < grids.length; i++) {
-    const currentGrid = grids[i];
-    for (let j = 0; j < currentGrid.length; j++) {
-        const currentGridRow = Math.floor(j / CELLS_PER_AXIS);
-        const currentGridColumn = j % CELLS_PER_AXIS;
-        if (currentGridColumn >= currentGridRow) {
-            const tempCellStorage = currentGrid[j];
-            currentGrid[j] = currentGrid[currentGridColumn * CELLS_PER_AXIS + currentGridRow];
-            currentGrid[currentGridColumn * CELLS_PER_AXIS + currentGridRow] = tempCellStorage;
-        }
-    }
-    grids[i] = currentGrid;
-    }
-    render(grids[grids.length-1]);
-}
-
 function render(grid) {
     for (let i = 0; i < grid.length; i++) {
         ctx.fillStyle = `rgb(${grid[i][0]}, ${grid[i][1]}, ${grid[i][2]})`;
@@ -112,24 +82,6 @@ function updateGridAt(mousePositionX, mousePositionY) {
         // Update the player's score
         updatePlayerScore();
     }
-}
-
-
-function updatePlayerScore() {
-playerScore = playerScore > 0 ? playerScore -= 1 : 0;
-}
-
-function floodFill(grid, gridCoordinate, colorToChange) {
-    if (arraysAreEqual(colorToChange, replacementColor)) { return }
-    else if (!arraysAreEqual(grid[gridCoordinate.row * CELLS_PER_AXIS + gridCoordinate.column], colorToChange)) { return }
-    else {
-        grid[gridCoordinate.row * CELLS_PER_AXIS + gridCoordinate.column] = replacementColor;
-        floodFill(grid, {column: Math.max(gridCoordinate.column - 1, 0), row: gridCoordinate.row}, colorToChange);
-        floodFill(grid, {column: Math.min(gridCoordinate.column + 1, CELLS_PER_AXIS - 1), row: gridCoordinate.row}, colorToChange);
-        floodFill(grid, {column: gridCoordinate.column, row: Math.max(gridCoordinate.row - 1, 0)}, colorToChange);
-        floodFill(grid, {column: gridCoordinate.column, row: Math.min(gridCoordinate.row + 1, CELLS_PER_AXIS - 1)}, colorToChange);
-    }
-    return;
 }
 
 function restart() {
@@ -165,14 +117,6 @@ function undoLastMove() {
     rollBackHistory();
 }
 
-rotateButton.addEventListener("mousedown", rotateGrid);
-function rotateGrid() {
-    transposeGrid();
-}
-
-colorSelectButtons.forEach(button => {
-    button.addEventListener("mousedown", () => replacementColor = CELL_COLORS[button.name])
-});
 
 // #endregion
 
@@ -188,24 +132,6 @@ function convertCartesiansToGrid(xPos, yPos) {
     };
 }
 
-// To choose a random property from a given object
-function chooseRandomPropertyFrom(object) {
-    const keys = Object.keys(object);
-    return object[keys[ Math.floor(keys.length * Math.random()) ]]; //Truncates to integer
-};
-
-// To compare two arrays
-function arraysAreEqual(arr1, arr2) {
-    if (arr1.length != arr2.length) { return false }
-    else {
-        for (let i = 0; i < arr1.length; i++) {
-            if (arr1[i] != arr2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
 
 // #endregion
 
